5893ddf4ded51f186dd84111b0943ea3e19c56eb
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-08-03 21:56:56 -0300

    feat:adding a image and fixing code.

--- cardapio/src/App.tsx
+++ cardapio/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Card } from './components/card/card';
-import { FoodData } from './interface/FoodData.ts';
+import { FoodData } from './interface/.ts';
 import { useFoodData } from './hooks/useFoodData';
 import { CreateModal } from './components/create-modal/create-modal';
 
